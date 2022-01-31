f7889d5e6138196cbbdfda2c8ab8ccfc1ce50eae
Aaron <aaroncourt09@gmail.com>
2022-01-31 15:46:32 -0800

    commit of completed assignment

--- flask/fundamentals/checkerboard/server.py
+++ flask/fundamentals/checkerboard/server.py
@@ -4,19 +4,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def eight_by_8():
-    return render_template('index.html', ht = 8, wd = 8)
+    return render_template('index.html', ht_num = int(8), wd_num = int(8))
 
 @app.route('/<int:wd_num>')
 def eight_by_4(wd_num):
-    return render_template('index.html', ht = 8, wd = wd_num)
+    print(type(wd_num))
+    return render_template('index.html', ht_num = int(8), wd_num = int(wd_num))
 
 @app.route('/<int:wd_num>/<int:ht_num>')
 def var_board(wd_num, ht_num):
-    return render_template('index.html', wd = wd_num, ht = ht_num)
+    return render_template('index.html', wd_num = int(wd_num), ht_num = int(ht_num))
 
-@app.route('/<int:wd_num>/<int:ht_num>/color1/color2')
+@app.route('/<int:wd_num>/<int:ht_num>/<string:color1>/<string:color2>')
 def var_color_board(wd_num, ht_num, color1, color2):
-    return render_template(wd = wd_num, ht = ht_num, color1 = color1, color2 = color2)
+    return render_template('index.html', wd_num = int(wd_num), ht_num = int(ht_num), color1 = color1, color2 = color2)
 
-if __name__=='_main__':
+if __name__=='__main__':
     app.run(debug=True)
